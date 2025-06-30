817edf7fd0639ff585f3249e1962b0ac522c0217
emadpoursina <emad.puursina@gmail.com>
2025-06-30 18:18:30 +0330

    add dotenv

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // require('app-module-path').addPath(__dirname)
 import App from "./app/index.js"
-// require('dotenv').config();
+import dotenv from "dotenv"
 import config from "./config/index.js"
+
 global.config = config
+dotenv.config()
 
 new App();
