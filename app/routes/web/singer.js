b68102ede4e6f2f917e9470803d1f19bca5e658a
emadpoursina <emad.puursina@gmail.com>
2025-07-04 09:15:24 +0330

    add /singer route

--- app/routes/web/singer.js
+++ app/routes/web/singer.js
@@ -1,10 +1,16 @@
 import express from 'express'
+import Singer from '../../models/singer.js'
 
 const router = express.Router();
 
 // Home Routes
-router.get('/' , (req, res) => {
-    res.send("singer");
+router.get('/' , async (req, res) => {
+    try {
+        const singers = await Singer.find();
+        res.json(singers);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch singers' });
+    }
 });
 
 export default router
