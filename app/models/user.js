506be5b22c048f3f43145c08d9e3f9df1e3e2c6a
emadpoursina <emad.puursina@gmail.com>
2025-07-04 12:32:49 +0330

    add some utility methods to user schema

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
-// const bcrypt = require('bcrypt');
-// const uniqueString = require('unique-string')
-// const mongoosePaginate = require('mongoose-paginate');
+import bcrypt from "bcrypt";
+import uniqueString from 'unique-string';
+import mongoosePaginate from 'mongoose-paginate'
 
 const Schema = mongoose.Schema;
 
@@ -20,25 +20,25 @@ const userSchema = new Schema({
     playlists: [playlistSchema],
 }, { timestamps: true, toJSON: { virtuals: true } });
 
-// userSchema.plugin(mongoosePaginate);
+userSchema.plugin(mongoosePaginate);
 
-// userSchema.methods.hashPassword = function(password) {
-//     let salt = bcrypt.genSaltSync(15);
-//     let hash = bcrypt.hashSync(password , salt);
+userSchema.methods.hashPassword = function(password) {
+    let salt = bcrypt.genSaltSync(15);
+    let hash = bcrypt.hashSync(password , salt);
 
-//     return hash;
-// }
+    return hash;
+}
 
-// userSchema.methods.comparePassword = function(password) {
-//     return bcrypt.compareSync(password , this.password);
-// }
+userSchema.methods.comparePassword = function(password) {
+    return bcrypt.compareSync(password , this.password);
+}
 
-// userSchema.methods.setRememberToken = function(res) {
-//     const token = uniqueString();
-//     res.cookie('remember_token' , token , { maxAge : 1000 * 60 * 60 * 24 * 90 , httpOnly : true , signed :true});
-//     this.updateOne({ rememberToken : token } , err => {
-//         if(err) console.log(err);
-//     });
-// }
+userSchema.methods.setRememberToken = function(res) {
+    const token = uniqueString();
+    res.cookie('remember_token' , token , { maxAge : 1000 * 60 * 60 * 24 * 90 , httpOnly : true , signed :true});
+    this.updateOne({ rememberToken : token } , err => {
+        if(err) console.log(err);
+    });
+}
 
 export default mongoose.model('User', userSchema)
