5d369c0616981c8b6831c3f5bb3af309407746a7
emadpoursina <emad.puursina@gmail.com>
2025-07-04 12:15:06 +0330

    remove some commented code

--- app/index.js
+++ app/index.js
@@ -1,12 +1,3 @@
-// const validator = require('express-validator');
-// const cookieParser = require('cookie-parser');
-// const session = require('express-session');
-// const flash = require('connect-flash');
-// const passport = require('passport');
-// const Helpers = require('./helpers');
-// const methodOverride = require('method-override');
-// const csrfErrorHandler = require('app/http/middleware/csrfErrorHandler');
-// const rememberLogin = require('app/http/middleware/rememberLogin');
 import express from "express"
 import http from 'http'
 import helmet from "helmet"
@@ -44,41 +35,17 @@ export default class Application {
      * Express Config
      */
     setConfig() {
-    //     require('app/passport/passport-local');
- 
-    //     app.enable('trust proxy'); 
         app.use(helmet());
         app.use(express.json());
         app.use(express.urlencoded({ extended: true }));
-    //     app.use(express.static(config.layout.public_dir));
-    //     app.set('view engine', config.layout.view_engine);
-    //     app.set('views' , config.layout.view_dir);
-    //     app.use(config.layout.ejs.expressLayouts);
-    //     app.set("layout extractScripts", config.layout.ejs.extractScripts);
-    //     app.set("layout extractStyles", config.layout.ejs.extractStyles);
-    //     app.set("layout" , config.layout.ejs.master);
-
 
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended : true }));
-    //     app.use(methodOverride('_method'));
-    //     app.use(validator());
-    //     app.use(session({...config.session}));
         app.use(cookieParser(config.get("cookie_secretkey")));
-    //     app.use(flash());
-    //     app.use(passport.initialize());
-    //     app.use(passport.session());
-    //     app.use(rememberLogin.handle);
-
-    //     app.use((req , res , next) => {
-    //         app.locals = new Helpers(req, res).getObjects();
-    //         next();
-    //     });
     }
 
     setRouters() {
         csrf({ cookie : true }) 
         app.use(router)
-        // app.use(csrfErrorHandler.handle);
     }
 }
