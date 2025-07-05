e5cd26ff8cdfb620176841a882b71f58052596ea
emadpoursina <emad.puursina@gmail.com>
2025-07-05 09:05:02 +0330

    Beautification

--- app/routes/web/index.js
+++ app/routes/web/index.js
@@ -1,52 +1,44 @@
-import express from 'express'
-import pageRouter from './page.js'
-import singerRouter from './singer.js'
-import albumRouter from './album.js'
-import genreRouter from './genre.js'
-import trackRouter from './track.js'
-import postRouter from './post.js'
-import authRouter from './auth.js'
-
-const router = express.Router();
+import express from "express";
+import pageRouter from "./page.js";
+import singerRouter from "./singer.js";
+import albumRouter from "./album.js";
+import genreRouter from "./genre.js";
+import trackRouter from "./track.js";
+import postRouter from "./post.js";
+import authRouter from "./auth.js";
+import adminRouter from "./admin.js";
 
 // Middlewares
-// const redirectIfAuthenticated = require('app/http/middleware/redirectIfAuthenticated');
+import redirectIfNotAuthenticated from "../../http/middlewares/redirectIfNotAuthenticated.js";
 // const redirectIfNotAuthenticated = require('app/http/middleware/redirectIfNotAuthenticated');
 // const redirectIfNotAdmin = require('app/http/middleware/redirectIfNotAdmin');
 // const errorHandler = require('app/http/middleware/errorHandler');
 
+const router = express.Router();
+
+// Single Page Router
+router.use("/", pageRouter);
+
 // Admin Router
-// const adminRouter = require('app/routes/web/admin');
-// router.use('/admin' , redirectIfNotAuthenticated.handle,  redirectIfNotAdmin.handle, adminRouter);
+router.use(
+  "/admin",
+  redirectIfNotAuthenticated.handle,
+  // redirectIfNotAdmin.handle,
+  adminRouter
+);
 
-// Home Router
-router.use('/' , pageRouter);
-
-// Singer Router
-router.use('/singer' , singerRouter);
-
-// Album Router
-router.use('/album', albumRouter);
-
-// Genre Router
-router.use('/genre', genreRouter);
-
-// Music Router
-router.use('/track', trackRouter);
-
-// Post Router
-router.use('/post', postRouter);
-
-// Dashboard Router
-// const dashboardRouter = require('app/routes/web/dashboard');
-// router.use('/dashboard' , dashboardRouter);
+// Models Router
+router.use("/singer", singerRouter);
+router.use("/album", albumRouter);
+router.use("/genre", genreRouter);
+router.use("/track", trackRouter);
+router.use("/post", postRouter);
 
 // Auth Router
-// router.use('/auth' , redirectIfAuthenticated.handle ,authRouter);
-router.use('/auth', authRouter);
+router.use("/auth", redirectIfNotAuthenticated.handle, authRouter);
 
 // Handle Errors
 // router.all('*' , errorHandler.error404);
 // router.use(errorHandler.handler)
 
-export default router
+export default router;
