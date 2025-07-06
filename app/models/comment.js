eb6cb58144ebc8d744fe54dd190b147c9cb93872
emadpoursina <emad.puursina@gmail.com>
2025-07-06 10:19:42 +0330

    add comment model

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,11 +1,29 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const commentSchema = new Schema({
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    track: { type: Schema.Types.ObjectId, ref: 'Track', required: true },
-    text: { type: String, required: true },
-}, { timestamps: { createdAt: true, updatedAt: false }, toJSON: { virtuals: true } });
+const commentSchema = new Schema(
+  {
+    content: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    commentableType: {
+      type: String,
+      enum: ["track", "album", "series", "movie"],
+      required: true,
+    },
+    commentableId: { type: Schema.Types.ObjectId, required: true },
+    parentComment: { type: Schema.Types.ObjectId, ref: "Comment" },
+    status: {
+      type: String,
+      enum: ["approved", "denied", "pending"],
+      default: "pending",
+    },
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: { createdAt: true, updatedAt: false },
+    toJSON: { virtuals: true },
+  }
+);
 
-export default mongoose.model('Comment', commentSchema) 
+export default mongoose.model("Comment", commentSchema);
